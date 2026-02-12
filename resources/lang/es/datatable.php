4bc07051577abc763740b02f67087b4d23abecc8
gonzda <dgonzalesruiz@gmail.com>
2026-02-12 16:08:40 -0500

    refactor: ommited translation

--- resources/lang/es/datatable.php
+++ resources/lang/es/datatable.php
@@ -43,7 +43,7 @@ return [
         'contains_not' => 'No contiene',
         'starts_with' => 'Comienza por',
         'ends_with' => 'Termina en',
-        'is_null' => 'És nulo',
+        'is_null' => 'Es nulo',
         'is_not_null' => 'No es nulo',
         'is_blank' => 'Es blanco',
         'is_not_blank' => 'No es blanco',
